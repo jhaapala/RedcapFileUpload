6020358599e3a7ca46e0d4648523b74fa9088370
Carlton Hanna <nullpointer0x00@gmail.com>
2015-09-19 17:43:06 -0500

    Still testing

--- src/RedcapUpload.groovy
+++ src/RedcapUpload.groovy
@@ -3,15 +3,15 @@ import geb.spock.GebSpec
 import org.openqa.selenium.firefox.FirefoxDriver
 import spock.lang.Specification
 
-public class RedcapUpload extends GebSpec{
+public class RedcapUpload extends GebSpec {
 
     def driver = new FirefoxDriver()
 
     def "test driver"(){
+
         setup:
-        go "http://www.google.com"
-        when:
-        println "Hello"
+        driver.get("http://www.google.com")
+
     }
 
 }
